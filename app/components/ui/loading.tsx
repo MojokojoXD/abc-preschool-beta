1b46d28ab8c7c33da237d268af2b7610353d1f93
mojokojoxd <kwadwoneer@gmail.com>
2025-04-01 14:03:09 -0600

    initial beta user test

--- app/components/ui/loading.tsx
+++ app/components/ui/loading.tsx
@@ -5,8 +5,8 @@ export function Loading()
 {
   return (
     <div className='fixed inset-0 bg-white z-50 w-full flex justify-center items-center'>
-      <div className='relative h-[20%] w-[20%] animate-pulse'>
-            <Image src={logo} alt='loading screen' className='object-fit' fill/> 
+      <div className='relative h-[20%] aspect-square animate-pulse'>
+            <Image src={logo} alt='loading screen' className='object-cover' fill/> 
       </div>
     </div>
   )
